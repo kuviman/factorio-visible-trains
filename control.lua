ff4a65f0a8ca6c1566fb593e40f75761641c3c67
kuviman <kuviman@gmail.com>
2026-06-22 21:28:39 +0400

    Better impl for drawing rails

--- control.lua
+++ control.lua
@@ -1,4 +1,22 @@
-local drawn_rails = {}
+local drawn = {}
+
+-- Queue implemented using two stacks
+Queue = {
+    new = function()
+        return { front = {}, back = {} }
+    end,
+    push = function(self, x)
+        table.insert(self.back, x)
+    end,
+    pop = function(self)
+        if #(self.front) == 0 then
+            while #(self.back) ~= 0 do
+                table.insert(self.front, table.remove(self.back))
+            end
+        end
+        return table.remove(self.front)
+    end,
+}
 
 function current_player()
     return game.players[1]
@@ -10,14 +28,35 @@ end
 
 function redraw_rail_graph()
     local surface = current_player().surface
-    for _, drawn in ipairs(drawn_rails) do
+    for _, drawn in ipairs(drawn) do
         drawn.destroy()
     end
-    drawn_rails = {}
+    drawn = {}
     if not settings.global["visible-trains-draw-rails"].value then
         return
     end
     local zoom_scale = current_zoom_scale()
+
+    function draw_line(from, to)
+        table.insert(drawn,
+            rendering.draw_line({
+                color = { 0.5, 0.5, 0.5, 1 },
+                width = 2 * zoom_scale,
+                from = from,
+                to = to,
+                surface = from.surface,
+                draw_on_ground = true,
+                render_mode = "chart",
+            })
+        )
+    end
+
+    -- store for each rail distance from closest keypoint and keypoint itself
+    -- keypoints are there in order to reduce amount of vertices for performance
+    local state = {}
+
+    -- queue for bfs
+    local q = Queue.new()
     for _, rail in ipairs(surface.find_entities_filtered(
         { name = {
             "curved-rail-a",
@@ -28,25 +67,56 @@ function redraw_rail_graph()
             "legacy-curved-rail",
         } }
     )) do
-        for _, rail_direction in pairs(defines.rail_direction) do
-            for rail_connection_direction_name, rail_connection_direction in pairs(defines.rail_connection_direction) do
-                if rail_connection_direction_name ~= "none" then
-                    local connected_rail = rail.get_connected_rail({
-                        rail_direction = rail_direction,
-                        rail_connection_direction = rail_connection_direction,
-                    })
-                    if connected_rail ~= nil then
-                        table.insert(drawn_rails,
-                            rendering.draw_line({
-                                color = { 1, 1, 1, 1 },
-                                width = 0.1 * zoom_scale,
-                                from = rail,
-                                to = connected_rail,
-                                surface = surface,
-                                draw_on_ground = true,
-                                render_mode = "chart",
+        if state[rail.unit_number] == nil then
+            state[rail.unit_number] = { distance = 0, keypoint = rail, direct_parent = rail }
+            log("START BFS FROM" .. serpent.block(rail))
+            Queue.push(q, rail)
+            while true do
+                rail = Queue.pop(q)
+                if rail == nil then
+                    break
+                end
+                local is_junction = false
+                local connected_rails = {}
+                for _, rail_direction in pairs(defines.rail_direction) do
+                    local connected_rails_in_this_direction = 0
+                    for rail_connection_direction_name, rail_connection_direction in pairs(defines.rail_connection_direction) do
+                        if rail_connection_direction_name ~= "none" then
+                            local connected_rail = rail.get_connected_rail({
+                                rail_direction = rail_direction,
+                                rail_connection_direction = rail_connection_direction,
                             })
-                        )
+                            if connected_rail ~= nil then
+                                connected_rails_in_this_direction = connected_rails_in_this_direction + 1
+                                table.insert(connected_rails, connected_rail)
+                            end
+                        end
+                    end
+                    if connected_rails_in_this_direction >= 2 then
+                        is_junction = true
+                    end
+                end
+                local is_keypoint = state[rail.unit_number].distance >= 10 or is_junction
+                if is_keypoint then
+                    if state[rail.unit_number].keypoint.unit_number ~= rail.unit_number then
+                        draw_line(rail, state[rail.unit_number].keypoint)
+                    end
+                    state[rail.unit_number] = {
+                        keypoint = rail,
+                        direct_parent = state[rail.unit_number].direct_parent,
+                        distance = 0,
+                    }
+                end
+                for _, connected_rail in ipairs(connected_rails) do
+                    if state[connected_rail.unit_number] == nil then
+                        Queue.push(q, connected_rail)
+                        state[connected_rail.unit_number] = {
+                            keypoint = state[rail.unit_number].keypoint,
+                            distance = state[rail.unit_number].distance + 1,
+                            direct_parent = rail,
+                        }
+                    elseif state[connected_rail.unit_number].keypoint.unit_number ~= state[rail.unit_number].keypoint.unit_number then
+                        draw_line(state[rail.unit_number].keypoint, state[connected_rail.unit_number].keypoint)
                     end
                 end
             end
